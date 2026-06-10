6f113219d1c9a4c2f3b539624525a69098b9e73c
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-06-10 23:17:57 +0530

    Initial commit

---
+++ Arrays .py
@@ -0,0 +1,19 @@
+# Creating an array (list)
+numbers = [10, 20, 30, 40, 50]
+
+# Accessing elements
+print(numbers[0])   # First element
+print(numbers[2])   # Third element
+
+# Adding elements
+numbers.append(60)
+
+# Removing elements
+numbers.remove(20)
+
+# Loop through array
+for num in numbers:
+    print(num)
+
+# Length of array
+print("Length:", len(numbers))
---
+++ README.md
@@ -0,0 +1,15 @@
+# Student Management System
+
+A Python + SQLite project for managing student records.
+
+## Features
+- Add Students
+- View Students
+- Search Students
+- Delete Students
+
+## Technologies Used
+- Python
+- SQLite
+- VS Code
+- GitHub
