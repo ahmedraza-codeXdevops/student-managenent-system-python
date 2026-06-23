5342b7d34e10f6ee2f6ad8d1ef9e570cd166bdc1
Ahmed Raza <ahmedrazak2511@gmail.com>
2026-06-23 20:01:00 +0530

    Add files via upload

---
+++ students marks.py
@@ -0,0 +1,11 @@
+marks = [85, 90, 78, 92, 88]
+
+total = sum(marks)
+average = total / len(marks)
+highest = max(marks)
+lowest = min(marks)
+
+print("Total Marks:", total)
+print("Average:", average)
+print("Highest:", highest)
+print("Lowest:", lowest)
